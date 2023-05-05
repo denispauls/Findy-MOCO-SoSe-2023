f0a4d74c6cf158ade3a4001f389d71c7785cfbe7
StepanKrivosheev <92262136+StepanKrivosheev@users.noreply.github.com>
2023-05-05 17:24:56 +0200

    First Wireframes

---
+++ app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUi()
+    }
+    private fun initUi(){
+
+
+        binding.usernamePhone.setOnClickListener{
+
+        }
+    }
+}
