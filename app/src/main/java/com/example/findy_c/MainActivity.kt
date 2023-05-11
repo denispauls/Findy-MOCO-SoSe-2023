35787e5dcf5a29ae775a138bd42b6ce448203a8c
StepanKrivosheev <92262136+StepanKrivosheev@users.noreply.github.com>
2023-05-11 18:42:17 +0200

    Friends Screen und Profile Card Composable

--- app/src/main/java/com/example/findy_c/MainActivity.kt
+++ app/src/main/java/com/example/findy_c/MainActivity.kt
@@ -1,31 +1,66 @@
 package com.example.FindY
 
+import android.content.ClipData.Item
 import android.os.Bundle
+import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MovableContent
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.awaitAll
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -98,7 +133,7 @@ fun Login() {
             .wrapContentSize(align = Alignment.BottomCenter)) {
 
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { },
                 modifier = Modifier.fillMaxWidth(),
                 shape = MaterialTheme.shapes.extraSmall,
                 colors = ButtonDefaults.buttonColors(
@@ -116,10 +151,61 @@ fun Login() {
         }
     }
 }
+@Composable
+fun ProfileCard() {
+    Card(
+        shape = RoundedCornerShape(4.dp),
+        colors = CardDefaults.cardColors(Color.Gray),
+        border = BorderStroke(1.dp, color = Color.Black),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+    ) {
+        Row {
+            Icon(
+                Icons.Rounded.AccountCircle,
+                contentDescription = null,
+                modifier = Modifier.size(60.dp)
+            )
+            Column(Modifier.padding(6.dp)) {
+                Text(
+                    text = "Name Surname",
+                    fontSize = 18.sp
+                )
+                Text(
+                    text = "Bio/Last online",
+                    fontSize = 14.sp
+                )
+            }
+        }
+    }
+}
+@Composable
+fun FriendsScreen(){
+    Surface(color = Color.Gray,
+        modifier = Modifier.verticalScroll(rememberScrollState())) {
+        }
+
+Box(Modifier.padding(top = 6.dp, start = 6.dp, end = 6.dp)){
+    LazyColumn(
+        Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(6.dp))
+    {
+        items(16){ProfileCard()
+        }
+    }
+    Icon(Icons.Rounded.MoreVert, null,
+        Modifier
+        .size(35.dp)
+        .align(Alignment.TopEnd))
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun Preview(){
-
-    Login()
+    FriendsScreen()
+    //ProfileCard()
+    //Login()
 }
