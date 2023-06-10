0c2d6a1685aa1fe04128b2fc38fc57cfd1943565
StepanKrivosheev <92262136+StepanKrivosheev@users.noreply.github.com>
2023-06-10 15:39:10 +0200

    Architectur

--- app/src/main/java/com/example/findy_c/ui/theme/NavGraph.kt
+++ app/src/main/java/com/example/findy_c/ui/theme/NavGraph.kt
@@ -1,36 +1,37 @@
-//package com.example.findy_c.ui.theme
-//
-//import androidx.compose.runtime.Composable
-//import androidx.navigation.NavHostController
-//import androidx.navigation.compose.NavHost
-//import androidx.navigation.compose.composable
-//import androidx.navigation.navigation
-//import com.example.findy_c.Screens
-//
-//@Composable
-//fun SetupNavGraph(
-//    navController: NavHostController
-//){
-//    NavHost(
-//        navController = navController,
-//        startDestination = Screens.Login.route
-//    ){
-//        composable(
-//            route = navigation.Screens.
-//        ){
-//           Login(navController = navController)
-//        }
-//        composable(
-//            route = Screens.Friends.route
-//        ){
-//            FriendsScreen(navController = navController)
-//        }
-//        composable(
-//            route = Screens.Map.route
-//        ){
-//            MapScreen()
-//        }
-//    }
-//}
+package com.example.findy_c.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.navigation
+import com.example.findy_c.Screens
+
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController
+){
+    NavHost(
+        navController = navController,
+        startDestination = Screens.Login.route
+    ){
+        composable(
+            route = Screens.Login.route
+        ){
+           Login(navController = navController)
+        }
+        composable(
+            route = Screens.Friends.route
+        ){
+            FriendsScreen(navController = navController)
+        }
+        composable(
+            route = Screens.Map.route
+        ){
+            MapScreen()
+        }
+    }
+}
 
 
